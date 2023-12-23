8fd986ccde314a6313525159529f2dd2d677d199
Sweet Van Loan <Sweet@SweetVanLoan.com>
2023-12-23 10:33:05 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,125 +1,106 @@
-#include <iostream>
-#include <string>
-#include <optional>
+import random
 
-//  Gender
-enum class Gender {
-    Male,
-    Female,
-    NonBinary
-};
+class CreatureCard:
+    def __init__(self, name, attack, defense, type):
+        self.name = name
+        self.attack = attack
+        self.defense = defense
+        self.type = type
 
-// Alignment
-enum class Alignment {
-    Good,
-    Neutral,
-    Evil
-};
+    def __str__(self):
+        return f"{self.name} (Attack: {self.attack}, Defense: {self.defense}, Type: {self.type})"
 
-// MetaHuman Class 
-class MetaHuman {
-public:
-    MetaHuman(const std::string& firstName, const std::string& lastName, const std::string& nickname, const std::string& power, Gender gender, Alignment alignment);
+class EnhancementCard:
+    def __init__(self, name, effect):
+        self.name = name
+        self.effect = effect
 
-    void activatePower() const;
-    void marry(MetaHuman& partner);
-    void divorce();
-    std::string getName() const;
-    std::string getNickname() const;
-    std::string describe() const;
-    bool isMarried() const;
+    def __str__(self):
+        return f"{self.name} (Effect: {self.effect})"
 
-private:
-    std::string firstName;
-    std::string lastName;
-    std::string nickname;
-    std::string power;
-    Gender gender;
-    Alignment alignment;
-    std::optional<std::string> spouseName;
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.deck = []
+        self.hand = []
+        self.active_enhancements = []
 
-    std::string genderToString() const;
-    std::string alignmentToString() const;
-};
+    def draw_card(self):
+        if self.deck:
+            card = self.deck.pop(0)
+            self.hand.append(card)
+            print(f"{self.name} drew {card}")
 
-//  Implementation
-MetaHuman::MetaHuman(const std::string& firstName, const std::string& lastName, const std::string& nickname, const std::string& power, Gender gender, Alignment alignment)
-    : firstName(firstName), lastName(lastName), nickname(nickname), power(power), gender(gender), alignment(alignment) {}
+    def play_card(self):
+        if self.hand:
+            return self.hand.pop(0)
+        else:
+            print(f"{self.name} has no cards to play!")
+            return None
 
-// Member Functions 
-void MetaHuman::activatePower() const {
-    std::cout << getName() << " (" << nickname << ") uses their power: " << power << std::endl;
-}
+def create_deck():
+    creatures = [
+        CreatureCard("Phoenix", 5, 4, "Fire"),
+        CreatureCard("Leviathan", 8, 7, "Water"),
+        CreatureCard("Dracula", 6, 5, "Dark"),
+        CreatureCard("Goliath", 7, 6, "Earth"),
+        CreatureCard("Kraken", 7, 6, "Water"),
+        CreatureCard("Dragon", 9, 7, "Fire"),
+        CreatureCard("Unicorn", 6, 5, "Light"),
+        CreatureCard("Wizard", 5, 4, "Arcane"),
+        CreatureCard("Werewolf", 7, 6, "Dark"),
+        # Add more creatures here
+    ]
 
-void MetaHuman::marry(MetaHuman& partner) {
-    if (!isMarried() && !partner.isMarried()) {
-        spouseName = partner.getName();
-        partner.spouseName = getName();
-    }
-}
+    enhancements = [
+        EnhancementCard("Mystic Forest", "+2 Defense to all creatures"),
+        EnhancementCard("Volcanic Terrain", "+3 Attack to Fire creatures"),
+        # Add more enhancements here
+    ]
 
-void MetaHuman::divorce() {
-    if (isMarried()) {
-        spouseName.reset();
-    }
-}
+    deck = creatures * 3 + enhancements * 2  # Adjust proportions as needed
+    random.shuffle(deck)
+    return deck
 
-std::string MetaHuman::getName() const {
-    return firstName + " " + lastName;
-}
+def apply_enhancements(player, card):
+    for enhancement in player.active_enhancements:
+        # Define the logic to apply enhancement effects here
+        print(f"Applying {enhancement.effect} to {card.name}")
 
-std::string MetaHuman::getNickname() const {
-    return nickname;
-}
+def battle(card1, card2):
+    print(f"{card1.name} battles {card2.name}!")
+    if card1.attack > card2.defense:
+        print(f"{card1.name} wins!")
+    elif card1.attack < card2.defense:
+        print(f"{card2.name} wins!")
+    else:
+        print("The battle is a draw!")
 
-std::string MetaHuman::describe() const {
-    std::string description = getName() + " (" + nickname + ") | Power: " + power + " | Gender: " + genderToString() + " | Alignment: " + alignmentToString();
-    if (isMarried()) {
-        description += " | Married to: " + *spouseName;
-    }
-    return description;
-}
+# Game Setup
+player1 = Player("Player 1")
+player2 = Player("Player 2")
+player1.deck = create_deck()
+player2.deck = create_deck()
 
-bool MetaHuman::isMarried() const {
-    return spouseName.has_value();
-}
+# Game Loop (for one round)
+for _ in range(5):
+    player1.draw_card()
+    player2.draw_card()
 
-std::string MetaHuman::genderToString() const {
-    switch (gender) {
-        case Gender::Male: return "Male";
-        case Gender::Female: return "Female";
-        case Gender::NonBinary: return "Non-Binary";
-        default: return "Unknown";
-    }
-}
+# Players play enhancement cards
+enhancement1 = player1.play_card()
+if isinstance(enhancement1, EnhancementCard):
+    player1.active_enhancements.append(enhancement1)
 
-std::string MetaHuman::alignmentToString() const {
-    switch (alignment) {
-        case Alignment::Good: return "Good";
-        case Alignment::Neutral: return "Neutral";
-        case Alignment::Evil: return "Evil";
-        default: return "Unknown";
-    }
-}
+enhancement2 = player2.play_card()
+if isinstance(enhancement2, EnhancementCard):
+    player2.active_enhancements.append(enhancement2)
 
-// Main 
-int main() {
-    MetaHuman hero("Morgan", "Blake", "Shadow", "Invisibility", Gender::Female, Alignment::Good);
-    MetaHuman partner("Jordan", "Reed", "Phoenix", "Telepathy", Gender::NonBinary, Alignment::Neutral);
+# Players play creature cards and battle
+card1 = player1.play_card()
+card2 = player2.play_card()
 
-    // Demo
-    std::cout << hero.describe() << std::endl;
-    std::cout << partner.describe() << std::endl;
-
-    hero.marry(partner);
-    std::cout << "\nAfter marriage:\n";
-    std::cout << hero.describe() << std::endl;
-    std::cout << partner.describe() << std::endl;
-
-    hero.divorce();
-    std::cout << "\nAfter divorce:\n";
-    std::cout << hero.describe() << std::endl;
-    std::cout << partner.describe() << std::endl;
-
-    return 0;
-}
+if card1 and card2:
+    apply_enhancements(player1, card1)
+    apply_enhancements(player2, card2)
+    battle(card1, card2)
